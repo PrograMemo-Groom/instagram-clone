f4c1cfb1df0f66536cf9bc4ac3444d9ff4bdb3fc
newfly101-friends <newfly101@newfriends.kr>
2024-12-22 08:54:55 +0900

    ➕ build시 리소스 못보게 변경(보안성)

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,18 @@ export default defineConfig({
   },
   server: {
     port: 3000
+  },
+  build: {
+    minify: "terser", // 압축 최적화
+    terserOptions: {
+      format: {
+        comments: false, // 주석 제거 옵션
+      },
+      compress: {
+        drop_console: false, // console 제거
+        drop_debugger: false, // debugger 제거
+      }
+    },
+    sourcemap: false, // source map 비활성화
   }
 })
