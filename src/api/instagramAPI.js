b221371c6f5d7002daca049f78688887267932f1
Jae-Hong-Kim <newfly101@naver.com>
2024-12-22 09:08:56 +0900

    ➕ instagram API 추가 - firebase-Oauth관련 처리 규정 확인하기
    Signed-off-by: newfly101-local <newfly101@naver.com>

---
+++ src/api/instagramAPI.js
@@ -0,0 +1,61 @@
+import {
+    INSTAGRAM_CLIENT_ID,
+    INSTAGRAM_CLIENT_SECRET,
+    INSTAGRAM_AUTH_URL,
+    INSTAGRAM_REDIRECT_URI,
+    INSTAGRAM_TOKEN_URL
+} from "@/config/config.js";
+import axios from "axios";
+
+const instagramInstance = axios.create({
+    baseURL: INSTAGRAM_TOKEN_URL,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+});
+
+/* Oauth 인증 url 생성 - firebase 연동시 변경 필요 */
+export const getAuthUrl = () => {
+    return `${INSTAGRAM_AUTH_URL}?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+}
+
+/* Access Token 교환 */
+export const exchangeAccessToken = async (code) => {
+    try {
+        const response = await instagramInstance.post("", null, {
+            params: {
+                client_id: INSTAGRAM_CLIENT_ID,
+                client_secret: INSTAGRAM_CLIENT_SECRET,
+                grant_type: "authorization_code",
+                redirect_uri: INSTAGRAM_REDIRECT_URI,
+                code: code,
+            },
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
+
+        // Access Token 및 사용자 ID 반환
+        return response.data;
+    } catch (error) {
+        console.error("Error exchanging access token:", error.response?.data || error.message);
+        throw error;
+    }
+};
+
+/* 사용자 프로필 가져오기 */
+export const getUserProfile = async (accessToken) => {
+    try {
+        const response = await axios.get("https://graph.instagram.com/me", {
+            params: {
+                fields: "id,username",
+                access_token: accessToken,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching user profile:", error.response?.data || error.message);
+        throw error;
+    }
+};
