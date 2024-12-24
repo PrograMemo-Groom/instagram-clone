4351dcaa0cffe749ecd9823ebf6d7cd38af7442d
S2ej1n <ks320205@naver.com>
2024-12-25 01:34:28 +0900

    💄 로고 찌그러짐 임시 수정

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full flex justify-between items-center
      bg-white border-b border-gray-300 px-4 py-2 z-50 md:hidden">
-      <div className="flex items-center">
+      <div className="flex items-center shrink-0 ">
         <button className="hover:bg-gray-100 rounded p-2">
           <img src="/img/icons/instagram_text_logo.png" 
-          alt="Instagram" className="h-7" />
+          alt="Instagram" className="w-[120px]" />
         </button>
       </div>
       <div className="flex items-center space-x-4">
@@ -15,7 +15,7 @@ const Header = () => {
           <input
             type="text"
             placeholder="검색"
-            className="h-9 w-56 rounded bg-gray-100 px-4 outline-none"
+            className="h-9 w-60 rounded bg-gray-100 px-4 outline-none"
           />
         </div>
         <img src="/img/icons/free-icon-heart-1077035.png" 
