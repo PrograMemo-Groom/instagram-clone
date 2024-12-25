4e7eabe9f7074af1128c608b2d9e9c2d6a3e25f1
saaad99 <ehdgnstla@naver.com>
2024-12-25 15:58:37 +0900

    :lipstick:게시글 아이콘 추가

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,11 +2,12 @@
 import data from './data.json';
 const Posts = () => {
     return (
-        <div className="flex flex-col p-4 w-full h-full bg-black text-white">
+        <div className="flex flex-col p-4 w-full h-full bg-black text-white border-t-gray-400">
             <div className="flex items-center justify-center gap-x-16 text-xs">
-                <span>게시물</span>
-                <span>릴스</span>
-                <span>태그됨</span>
+                <span className="p-2 flex gap-1">
+                    <img src="assets/icon/posts_icon.svg"/>게시물</span>
+                <span className="p-2 bg-amber-300">릴스</span>
+                <span className="p-2 bg-amber-300">태그됨</span>
             </div>
             <div className="grid grid-cols-3 grid-rows-3 gap-1">
                 {data.map((data, index) => {
