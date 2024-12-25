b63ff767b989cc012eead184422eb94100a70e59
happygirlll <lbm000314@gmail.com>
2024-12-25 11:45:25 +0900

    💄컨테이너 크기 조정

--- src/pages/Reels.jsx
+++ src/pages/Reels.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Reels = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="w-[400px] h-[730px] bg-gray-300 rounded  overflow-hidden relative">
+            <div className="w-[440px] h-[780px] bg-gray-300 rounded  overflow-hidden relative">
                 <div className="absolute bottom-0 left-0 w-full p-4">
                     <div className="flex items-center">
                         <img
