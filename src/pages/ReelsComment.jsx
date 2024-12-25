2a51a9dce06d0c3ba60d7ab1945c7e2831c3f0a9
happygirlll <lbm000314@gmail.com>
2024-12-25 21:17:25 +0900

    💄댓글창 댓글목록, 댓글입력창 추가

--- src/pages/ReelsComment.jsx
+++ src/pages/ReelsComment.jsx
@@ -7,7 +7,7 @@ const ReelsComment = ({ onClose }) => {
         ">
             <div className="flex items-center justify-between mb-4 relative">
                 <button
-                    className="absolute left-0 top-1/2 transform -translate-y-1/2 "
+                    className="absolute left-3 top-1/2 transform -translate-y-1/2 "
                     onClick={onClose}
                 >
                     ✕
@@ -16,6 +16,43 @@ const ReelsComment = ({ onClose }) => {
                 <h2 className="text-base font-bold text-center flex-1">댓글</h2>
 
             </div>
+
+            <div className="overflow-y-auto flex-1 mb-4">
+                <div className="mb-4">
+                    <p className="font-bold">사용자1</p>
+                    <p className="text-sm ">댓글 내용 1</p>
+                </div>
+                <div className="mb-4">
+                    <p className="font-bold">사용자2</p>
+                    <p className="text-sm ">댓글 내용 2</p>
+                </div>
+                <div className="mb-4">
+                    <p className="font-bold">사용자3</p>
+                    <p className="text-sm ">댓글 내용 3</p>
+                </div>
+                <div className="mb-4">
+                    <p className="font-bold">사용자4</p>
+                    <p className="text-sm ">댓글 내용 3</p>
+                </div>
+                <div className="mb-4">
+                    <p className="font-bold">사용자5</p>
+                    <p className="text-sm ">댓글 내용 3</p>
+                </div>
+            </div>
+
+            <div className="flex items-center bg-gray-100 rounded-full p-1 border">
+                <img
+                    src=""
+                    alt="프로필"
+                    className="w-8 h-8 rounded-full mr-3"
+                />
+                <input
+                    type="text"
+                    placeholder="댓글 달기..."
+                    className="flex-1 bg-transparent outline-none text-sm"
+                />
+            </div>
+
         </div>
 
     );
