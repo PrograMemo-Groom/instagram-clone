cbca04977893fd46425084fead0ce3f6ab6746e9
saaad99 <ehdgnstla@naver.com>
2024-12-25 17:57:17 +0900

    :lipstick: redux 상태변수 isDark 추가

--- src/store/action/MainAction.js
+++ src/store/action/MainAction.js
@@ -1,6 +1,11 @@
-import {SET_MAIN_CONTENT} from "@/store/type/MainActionType.js";
+import {SET_IS_DARK, SET_MAIN_CONTENT} from "@/store/type/MainActionType.js";
 
 export const setMainResult = (mainResult) => ({
     type: SET_MAIN_CONTENT,
     payload: mainResult,
+})
+
+export const setIsDark = (isDark) => ({
+    type: SET_IS_DARK,
+    payload: isDark,
 })
--- src/store/reducer/MainReducer.js
+++ src/store/reducer/MainReducer.js
@@ -1,11 +1,17 @@
 const initialState = {
     mainResult: [],
+    isDark : false,
 }
 
 const MainReducer = (state=initialState, action) => {
     switch(action.type) {
         case 'SET_MAIN_CONTENT':
             return {...state, mainResult: action.payload}
+        case 'SET_IS_DARK' :
+            return {
+                ...state,
+                isDark : action.payload
+            }
         default:
             return state;
     }
