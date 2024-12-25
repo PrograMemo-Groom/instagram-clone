df0cefc38f67914bb2688ec36b83cee0bc926f89
saaad99 <ehdgnstla@naver.com>
2024-12-25 16:26:06 +0900

    :lipstick: 스크롤바 디자인 숨김

--- src/components/savedStories/SavedStories.jsx
+++ src/components/savedStories/SavedStories.jsx
@@ -3,10 +3,16 @@ import data from './data.json';
 
 const SavedStories = () => {
     return (
-        <div className="flex flex-nowrap gap-x-10 overflow-x-scroll justify-items-start p-4 w-full bg-black ">
+        <div
+            style={{
+                msOverflowStyle: "none", // IE and Edge
+                scrollbarWidth: "none"   // Firefox
+            }}
+            className="flex flex-nowrap gap-x-10 overflow-x-auto scrollbar-hide justify-items-start p-4 w-full bg-black">
             {data.map((data, index) => {
                 return (
-                    <div key={index} className="relative flex flex-col flex-shrink-0 justify-evenly text-white items-center cursor-pointer">
+                    <div key={index}
+                         className="relative flex flex-col flex-shrink-0 justify-evenly text-white items-center cursor-pointer">
                         <img className="w-[85px] h-[85px] rounded-full" src={data.src}/>
                         <span>{data.text}</span>
                     </div>
