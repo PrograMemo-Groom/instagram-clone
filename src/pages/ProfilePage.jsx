2edc6319328461890b66967365b6e29fad5a4fac
saaad99 <ehdgnstla@naver.com>
2024-12-25 12:53:38 +0900

    :lipstick: 보관된 스토리 컴포넌트 프로필 페이지에 추가

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,13 @@
 // import React from 'react';
 
 import UserSection from "@/components/UserSection.jsx";
+import SavedStories from "@/components/SavedStories.jsx";
 
 const ProfilePage = () => {
     return (
         <div className="m-16 p-4">
             <UserSection/>
+            <SavedStories/>
 
         </div>
     );
