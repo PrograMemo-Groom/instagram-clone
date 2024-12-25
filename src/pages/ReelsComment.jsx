368f65c8b63db6d879a2731e24532db36f957454
happygirlll <lbm000314@gmail.com>
2024-12-25 20:45:37 +0900

    💄댓글창 미디어쿼리 적용

--- src/pages/ReelsComment.jsx
+++ src/pages/ReelsComment.jsx
@@ -1,6 +1,10 @@
 const ReelsComment = ({ onClose }) => {
     return (
-        <div className="absolute top-[40px] left-[-320px] bg-white w-[350px] h-[500px] rounded-lg shadow-lg z-50">
+        <div className="absolute top-[40px] left-[-320px] bg-white rounded-lg shadow-lg z-50
+            lg:w-[350px] lg:h-[500px]
+            sm:w-[350px] sm:h-[470px]
+            w-[350px] h-[370px]
+        ">
             <button
                 className="absolute top-4 left-4 "
                 onClick={onClose}
