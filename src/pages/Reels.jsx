4c742e239464514ef1f69efbb8f7b9045bb43a7e
happygirlll <lbm000314@gmail.com>
2024-12-25 15:47:13 +0900

    💄커서 추가

--- src/pages/Reels.jsx
+++ src/pages/Reels.jsx
@@ -4,22 +4,22 @@ const Reels = () => {
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="relative flex">
-                <div className="relative w-[440px] h-[780px] bg-gray-300 rounded overflow-hidden">
+                <div className="relative w-[440px] h-[780px] bg-gray-300 rounded overflow-hidden cursor-pointer">
                     <div className="absolute bottom-0 left-0 w-full p-4">
                         <div className="flex items-center">
                             <img
                                 src="/assets/reels_profile.png"
                                 alt="Profile"
-                                className="w-8 h-8 rounded-full"
+                                className="w-8 h-8 rounded-full cursor-pointer"
                             />
                             <div className="ml-3">
-                                <p className="font-bold text-white text-sm">사용자 id</p>
+                                <p className="font-bold text-white text-sm cursor-pointer">사용자 id</p>
                             </div>
                             <div className="ml-3">
                                 <p className="text-white"> • </p>
                             </div>
                             <div className="ml-3">
-                                <button className="font-bold text-white text-sm border rounded-lg px-1.5 py-1">팔로우</button>
+                                <button className="font-bold text-white text-sm border rounded-lg px-1.5 py-1 cursor-pointer">팔로우</button>
                             </div>
                         </div>
 
@@ -28,7 +28,7 @@ const Reels = () => {
                         </div>
 
                         <div className="mt-3 flex items-center bg-gray-500 bg-opacity-50 p-2 rounded-3xl">
-                            <div className="text-white text-sm">노래노래</div>
+                            <div className="text-white text-sm cursor-pointer">노래노래</div>
                         </div>
                     </div>
                 </div>
@@ -38,7 +38,7 @@ const Reels = () => {
                         <img
                             src="/assets/like_btn.svg"
                             alt="Like"
-                            className="w-6 h-6"
+                            className="w-6 h-6 cursor-pointer"
                         />
                         <p className="text-xs">5.8만</p>
                     </div>
@@ -47,7 +47,7 @@ const Reels = () => {
                         <img
                             src="/assets/comment_btn.svg"
                             alt="Comment"
-                            className="w-6 h-6 mt-2"
+                            className="w-6 h-6 mt-2 cursor-pointer"
                         />
                         <p className="text-xs">223</p>
                     </div>
@@ -56,7 +56,7 @@ const Reels = () => {
                         <img
                             src="/assets/share_btn.svg"
                             alt="Share"
-                            className="w-6 h-6 mt-2"
+                            className="w-6 h-6 mt-2 cursor-pointer"
                         />
                     </div>
 
@@ -64,7 +64,7 @@ const Reels = () => {
                         <img
                             src="/assets/bookmark_btn.svg"
                             alt="Bookmark"
-                            className="w-6 h-6 mt-3"
+                            className="w-6 h-6 mt-3 cursor-pointer"
                         />
                     </div>
 
@@ -72,7 +72,7 @@ const Reels = () => {
                         <img
                             src="/assets/menu_btn.svg"
                             alt="Menu"
-                            className="w-5 h-5 mt-3"
+                            className="w-5 h-5 mt-3 cursor-pointer"
                         />
                     </div>
 
@@ -80,7 +80,7 @@ const Reels = () => {
                         <img
                             src="/assets/song.png"
                             alt="Song"
-                            className="w-7 h-7 mt-4 rounded border-1 border-black"
+                            className="w-7 h-7 mt-4 rounded border-1 border-black cursor-pointer"
                         />
                     </div>
                 </div>
