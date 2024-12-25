018e2065a953c452577549a922bc27ecef5c291b
saaad99 <ehdgnstla@naver.com>
2024-12-25 16:27:27 +0900

    :lipstick: 프로필 화면 ui 간략히 작성 완료

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,19 +1,22 @@
 // import React from 'react';
 import data from './data.json';
+
 const Posts = () => {
     return (
         <div className="flex flex-col p-4 w-full h-full bg-black text-white border-t-gray-400">
             <div className="flex items-center justify-center gap-x-16 text-xs">
-                <span className="p-2 flex gap-1">
+                <span className="p-2 flex gap-1 items-center">
                     <img src="assets/icon/posts_icon.svg"/>게시물</span>
-                <span className="p-2 bg-amber-300">릴스</span>
-                <span className="p-2 bg-amber-300">태그됨</span>
+                <span className="p-2 flex gap-1 items-center">
+                    <img src="/assets/icon/reels_icon.svg"/>릴스</span>
+                <span className="p-2 flex gap-1 items-center">
+                    <img src="/assets/icon/taged_icon.svg"/>태그됨</span>
             </div>
             <div className="grid grid-cols-3 grid-rows-3 gap-1">
                 {data.map((data, index) => {
                     return (
                         <div className="h-[308px]" key={index}>
-                            <img className="w-full h-full object-cover" src={data.thumnailSrc}/>
+                            <img className="w-full h-full object-cover cursor-pointer" src={data.thumnailSrc}/>
                         </div>
                     );
                 })}
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,13 +2,14 @@
 
 import UserSection from "@/components/userSection/UserSection.jsx";
 import SavedStories from "@/components/savedStories/SavedStories.jsx";
+import Posts from "@/components/posts/Posts.jsx";
 
 const ProfilePage = () => {
     return (
         <div className="m-16 p-4 w-[935px]">
             <UserSection/>
             <SavedStories/>
-
+            <Posts/>
         </div>
     );
 };
