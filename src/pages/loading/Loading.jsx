0fd57a83f7c15fce0fbb19d8dba6bfc385575410
newfly101-local <newfly101@naver.com>
2024-12-25 10:56:13 +0900

    ✨ feat: Loading 페이지 [common page 구현] resolve: #7 related to : #10 #11
    Signed-off-by: newfly101-local <newfly101@naver.com>

--- src/pages/loading/Loading.jsx
+++ src/pages/loading/Loading.jsx
@@ -3,11 +3,12 @@ import logo from "@/assets/instagram1024.png";
 
 const Loading = () => {
     return (
-        <div className="inline-flex flex-col items-center justify-center h-full">
-            <div className="flex flex-col items-center justify-center w-[50px] h-[calc(100vh-30px)] margin-auto">
+        <div className="inline-flex flex-col items-center justify-between min-h-[80px]">
+            <div className="flex items-center justify-center w-[50px] h-[calc(100vh-50px)] mx-auto">
                 <img className="w-[50px] h-[50px]" src={logo} alt="instagram logo"/>
             </div>
-            <p className="h-[30px]">Loading...</p>
+            <p className="text-xl h-[30px]">Loading...</p>
+            <p className="text-xs h-[10px]"></p>
         </div>
     );
 };
