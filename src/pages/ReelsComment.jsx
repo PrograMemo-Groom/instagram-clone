433eec57d489e4c64532a6de158c0d16ea831215
happygirlll <lbm000314@gmail.com>
2024-12-25 20:56:41 +0900

    💄댓글창 제목 추가 및 배치 조정

--- src/pages/ReelsComment.jsx
+++ src/pages/ReelsComment.jsx
@@ -1,18 +1,23 @@
 const ReelsComment = ({ onClose }) => {
     return (
-        <div className="absolute top-[40px] left-[-320px] bg-white rounded-lg shadow-lg z-50
+        <div className="absolute top-[40px] left-[-320px] bg-white rounded-lg shadow-lg z-50 p-6
             lg:w-[350px] lg:h-[500px]
             sm:w-[350px] sm:h-[470px]
             w-[350px] h-[370px]
         ">
-            <button
-                className="absolute top-4 left-4 "
-                onClick={onClose}
-            >
-                ✕
-            </button>
+            <div className="flex items-center justify-between mb-4 relative">
+                <button
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 "
+                    onClick={onClose}
+                >
+                    ✕
+                </button>
 
+                <h2 className="text-base font-bold text-center flex-1">댓글</h2>
+
+            </div>
         </div>
+
     );
 };
 
