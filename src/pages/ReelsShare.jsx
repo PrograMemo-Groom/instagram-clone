c395ab7c7b71edb20e44d0a3a525d7bdd98fc954
happygirlll <lbm000314@gmail.com>
2024-12-26 21:57:30 +0900

    💄공유창 하단에 이전, 다음 버튼 추가

--- src/pages/ReelsShare.jsx
+++ src/pages/ReelsShare.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 const ReelsShare = ({ onClose }) => {
     const [isFocused, setIsFocused] = useState(false);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
 
     const handleFocus = () => {
         setIsFocused(true);
@@ -10,6 +12,23 @@ const ReelsShare = ({ onClose }) => {
 
     const handleCancel = () => {
         setIsFocused(false);
+    };
+
+    const handleNext = () => {
+        const scrollContainer = document.getElementById("share-scroll-container");
+        if (scrollContainer) {
+            const maxScrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+            setScrollPosition((prev) => Math.min(prev + 300, maxScrollLeft));
+            scrollContainer.scrollLeft = Math.min(scrollPosition + 100, maxScrollLeft);
+        }
+    };
+
+    const handlePrev = () => {
+        const scrollContainer = document.getElementById("share-scroll-container");
+        if (scrollContainer) {
+            setScrollPosition((prev) => Math.max(prev - 300, 0));
+            scrollContainer.scrollLeft = Math.max(scrollPosition - 300, 0);
+        }
     };
 
     return (
@@ -176,106 +195,126 @@ const ReelsShare = ({ onClose }) => {
 
                 </div>
 
-                <div className="flex justify-start border-t py-4 mb-3 px-5">
-                    <div className="flex flex-col items-center mr-6">
-                        <button className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center">
-                            <img
-                                src="/assets/reels/link.svg"
-                                alt="링크복사"
-                                className="w-5 h-5"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">링크 복사</p>
+                <div className="flex justify-between items-center border-t py-4 px-5 relative">
+                    <button
+                        className="font-bold text-white p-2 w-7 h-7 rounded-full bg-gray-600 bg-opacity-70 flex justify-center items-center absolute top-1/2 left-2 transform -translate-y-1/2 z-10"
+                        onClick={handlePrev}
+                    >
+                        ＜
+                    </button>
+
+                    <div
+                        id="share-scroll-container"
+                        className="flex overflow-x-scroll gap-4 scrollbar-hide w-full"
+                    >
+                        <div className="flex flex-col items-center mr-6">
+                            <button className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center">
+                                <img
+                                    src="/assets/reels/link.svg"
+                                    alt="링크복사"
+                                    className="w-5 h-5"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">링크 복사</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://www.facebook.com/sharer/sharer.php?app_id=1217981644879628&u=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DMjAyZTYwOWFjOA%3D%3D'}
+                            >
+                                <img
+                                    src="/assets/reels/facebook.svg"
+                                    alt="페이스북"
+                                    className="w-6 h-6"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">Facebook</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6 ">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://www.facebook.com/login.php?api_key=1217981644879628&skip_api_login=1&display=page&cancel_url=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DM2ViY2RjMDFkMA%3D%3D&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fsend%3F_path%3Dsend%26app_id%3D1217981644879628%26client_id%3D1217981644879628%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Freel%252FDEAU-H-T7Ff%252F%253Figsh%253DM2ViY2RjMDFkMA%253D%253D%26display%3Dpage%26link%3Dhttps%253A%252F%252Fwww.instagram.com%252Freel%252FDEAU-H-T7Ff%252F%253Figsh%253DM2ViY2RjMDFkMA%253D%253D%26from_login%3D1'}
+                            >
+                                <img
+                                    src="/assets/reels/messenger.svg"
+                                    alt="메신저"
+                                    className="w-6 h-6"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">Messenger</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://api.whatsapp.com/send/?text=%40vkvkvk_11%EB%8B%98%EC%9D%98+%EC%9D%B4+Instagram+%EA%B2%8C%EC%8B%9C%EB%AC%BC+%EB%B3%B4%EA%B8%B0%3A+https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DYzAyMDM1MGJkZA%3D%3D&type=custom_url&app_absent=0'}
+                            >
+                                <img
+                                    src="/assets/reels/whatsapp.svg"
+                                    alt="whatsApp"
+                                    className="w-5 h-5"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">WhatsApp</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://mail.google.com/mail'}
+                            >
+                                <img
+                                    src="/assets/reels/email.svg"
+                                    alt="이메일"
+                                    className="w-5 h-5"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">Email</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://www.threads.net/login?hl=ko'}
+                            >
+                                <img
+                                    src="/assets/reels/threads.svg"
+                                    alt="스레드"
+                                    className="w-5 h-5"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">Threads</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button
+                                className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
+                                onClick={() => window.location.href = 'https://x.com/intent/post?text=%40vkvkvk_11%EB%8B%98%EC%9D%98+%EC%9D%B4+Instagram+%EA%B2%8C%EC%8B%9C%EB%AC%BC+%EB%B3%B4%EA%B8%B0&url=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DY2Q0NmNiMjc3NQ%3D%3D'}
+                            >
+                                <img
+                                    src="/assets/reels/x.svg"
+                                    alt="x"
+                                    className="w-5 h-5"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">X</p>
+                        </div>
+                        <div className="flex flex-col items-center mr-6">
+                            <button className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center">
+                                <img
+                                    src="/assets/reels/share.svg"
+                                    alt="모두보기"
+                                    className="w-4 h-4"
+                                />
+                            </button>
+                            <p className="text-xs mt-2">모두보기</p>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://www.facebook.com/sharer/sharer.php?app_id=1217981644879628&u=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DMjAyZTYwOWFjOA%3D%3D'}
-                        >
-                            <img
-                                src="/assets/reels/facebook.svg"
-                                alt="페이스북"
-                                className="w-6 h-6"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">Facebook</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6 ">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://www.facebook.com/login.php?api_key=1217981644879628&skip_api_login=1&display=page&cancel_url=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DM2ViY2RjMDFkMA%3D%3D&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fsend%3F_path%3Dsend%26app_id%3D1217981644879628%26client_id%3D1217981644879628%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Freel%252FDEAU-H-T7Ff%252F%253Figsh%253DM2ViY2RjMDFkMA%253D%253D%26display%3Dpage%26link%3Dhttps%253A%252F%252Fwww.instagram.com%252Freel%252FDEAU-H-T7Ff%252F%253Figsh%253DM2ViY2RjMDFkMA%253D%253D%26from_login%3D1'}
-                        >
-                            <img
-                                src="/assets/reels/messenger.svg"
-                                alt="메신저"
-                                className="w-6 h-6"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">Messenger</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://api.whatsapp.com/send/?text=%40vkvkvk_11%EB%8B%98%EC%9D%98+%EC%9D%B4+Instagram+%EA%B2%8C%EC%8B%9C%EB%AC%BC+%EB%B3%B4%EA%B8%B0%3A+https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DYzAyMDM1MGJkZA%3D%3D&type=custom_url&app_absent=0'}
-                        >
-                            <img
-                                src="/assets/reels/whatsapp.svg"
-                                alt="whatsApp"
-                                className="w-5 h-5"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">WhatsApp</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://mail.google.com/mail'}
-                        >
-                            <img
-                                src="/assets/reels/email.svg"
-                                alt="이메일"
-                                className="w-5 h-5"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">Email</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://www.threads.net/login?hl=ko'}
-                        >
-                            <img
-                                src="/assets/reels/threads.svg"
-                                alt="스레드"
-                                className="w-5 h-5"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">Threads</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button
-                            className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                            onClick={() => window.location.href = 'https://x.com/intent/post?text=%40vkvkvk_11%EB%8B%98%EC%9D%98+%EC%9D%B4+Instagram+%EA%B2%8C%EC%8B%9C%EB%AC%BC+%EB%B3%B4%EA%B8%B0&url=https%3A%2F%2Fwww.instagram.com%2Freel%2FDEAU-H-T7Ff%2F%3Figsh%3DY2Q0NmNiMjc3NQ%3D%3D'}
-                        >
-                            <img
-                                src="/assets/reels/x.svg"
-                                alt="x"
-                                className="w-5 h-5"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">X</p>
-                    </div>
-                    <div className="flex flex-col items-center mr-6">
-                        <button className="w-14 h-14 rounded-full bg-gray-100 flex justify-center items-center">
-                            <img
-                                src="/assets/reels/share.svg"
-                                alt="모두보기"
-                                className="w-4 h-4"
-                            />
-                        </button>
-                        <p className="text-xs mt-2">모두보기</p>
-                    </div>
+
+                    <button
+                        className="font-bold text-white p-2 w-7 h-7 rounded-full bg-gray-600 bg-opacity-70 flex justify-center items-center absolute top-1/2 right-2 transform -translate-y-1/2 z-10"
+                        onClick={handleNext}
+                    >
+                        ＞
+                    </button>
                 </div>
+
             </div>
         </div>
     );
