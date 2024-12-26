81f28b85d38fa1998459cd609e06fdac3da0b27e
happygirlll <lbm000314@gmail.com>
2024-12-26 11:43:41 +0900

    💄공유창 헤더 부분 생성

--- src/pages/ReelsShare.jsx
+++ src/pages/ReelsShare.jsx
@@ -1,7 +1,17 @@
-const ReelsShare = () => {
+const ReelsShare = ({}) => {
     return (
-        <div>
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+            <div className="bg-white w-[550px] rounded-lg shadow-lg">
+                <div className="flex items-center justify-between px-4 py-2 border-b">
+                    <h2 className="text-center font-bold flex-1">공유</h2>
+                    <button
+                        className="font-bold text-lg"
+                    >
+                        ✕
+                    </button>
+                </div>
 
+            </div>
         </div>
     );
 };
