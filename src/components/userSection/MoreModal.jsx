85c98fdcbac5df8bedddcc09811ed6146f35e396
saaad99 <ehdgnstla@naver.com>
2024-12-26 17:39:21 +0900

    :lipstick: 화면 모드에 따른 디자인 변경

--- src/components/userSection/MoreModal.jsx
+++ src/components/userSection/MoreModal.jsx
@@ -11,7 +11,6 @@ const MoreModal = () => {
         { text: "메시지 보내기", onClick: null },
         { text: "취소", style: "text-blue-500 font-bold", onClick: "toggleModal" },
     ];
-
     const { modalIsOpen } = useSelector((state) => state.profile);
     const dispatch = useDispatch();
 
@@ -20,13 +19,13 @@ const MoreModal = () => {
     };
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white rounded-lg shadow-lg w-80">
-                <ul className="divide-y divide-gray-200">
+        <div className="bg-black fixed inset-0  bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white dark:bg-gray-700 rounded-lg shadow-lg w-80">
+                <ul className="divide-y divide-gray-200 dark:divide-gray-600">
                     {options.map((option, index) => (
                         <li
                             key={index}
-                            className={`py-3 px-4 text-center cursor-pointer hover:bg-gray-100 ${option.style || ""}`}
+                            className={`py-3 px-4 text-center cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-400 ${option.style || ""}`}
                             onClick={option.onClick === "toggleModal" ? toggleModal : undefined}
                         >
                             {option.text}
