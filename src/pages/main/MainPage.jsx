b68c90910e974d7ba161ff4d959eec5804daf153
S2ej1n <ks320205@naver.com>
2024-12-27 18:52:27 +0900

    🔧 feat:메인 - 파일 구성 변경 (main 폴더 생성)

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -4,10 +4,12 @@ const MainPage = () => {
     return (
         // 메인 - (스토리, 포스트) / 오른쪽 사이드바) 
         <main className="flex">
-            <section style={{backgroundColor:"lightblue"}}> 
+            <section className="w-[630px]"
+            style={{backgroundColor:"lightblue"}}> 
                 왼쪽 섹션 
+                <p>이다</p>
             </section>
-            <aside className="hidden rightbarpoint:block" 
+            <aside className="w-[319px] ml-[64px] hidden rightbarpoint:block" 
             style={{backgroundColor:"pink"}}>
                 오른쪽 사이드바
                 <footer>
