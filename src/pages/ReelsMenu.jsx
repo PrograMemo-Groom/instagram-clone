9ce5f9a9dcecc9355357add93aa00de96ca30aad
happygirlll <lbm000314@gmail.com>
2024-12-27 16:55:03 +0900

    💄릴스 메뉴창 미디어쿼리 적용

--- src/pages/ReelsMenu.jsx
+++ src/pages/ReelsMenu.jsx
@@ -10,7 +10,13 @@ const ReelsMenu = () => {
     ];
 
     return (
-        <div className="absolute top-[490px] left-[-150px] inset-0 z-50 w-[330px] flex justify-center items-center">
+        <div className="absolute inset-0 z-50 w-[330px] flex justify-center items-center
+                        lg:top-[490px] lg:left-[10px]
+                        md:top-[420px] md:left-[-190px]
+                        sm:top-[420px] sm:left-[-220px]
+                        top-[330px] left-[-250px]
+
+        ">
 
             <div className="bg-white rounded-2xl shadow-lg w-[90%] max-w-[350px] p-4">
                 <ul>
