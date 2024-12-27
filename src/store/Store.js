79c2c1cec88f89a89e1d9bb2b9b860f867380bfd
jdaily00 <kyeonghee_@naver.com>
2024-12-27 16:25:17 +0900

    🎨 Feat[#2]: search store 추가

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import mainReducer from "@/store/reducer/MainReducer.js";
 import commonReducer from "@/store/reducer/CommonReducer.js";
+import searchReducer from "@/components/searchTab/searchSlice.js"
 
 export const Store = configureStore({
     reducer: {
         main: mainReducer,
         common: commonReducer,
+        search: searchReducer,
     }
-
 });
 
 export default Store;
