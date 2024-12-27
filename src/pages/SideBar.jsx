ff1e059c22961e7473f99117d65942f806fdb6b0
jdaily00 <kyeonghee_@naver.com>
2024-12-27 16:13:38 +0900

    🎨 Feat[#1]: 사이드바에 onClick 이벤트 추가

--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import {toggleSearch} from "@/components/search/searchSlice.js";
 
 const SideBar = () => {
+    const dispatch = useDispatch();
+
     const menuItems = [
         { label: '홈', icon: '/img/icons/free-icon-home-5948524.png' },
-        { label: '검색', icon: '/img/icons/free-icon-search-5948534.png' },
+        { label: '검색', icon: '/img/icons/free-icon-search-5948534.png',onClick: () => dispatch(toggleSearch()) },
         { label: '탐색 탭', icon: '/img/icons/free-icon-compass-2948031.png' },
         { label: '릴스', icon: '/img/icons/free-icon-video-5948543.png' },
         { label: '메세지', icon: '/img/icons/free-icon-direct-instagram-5883507.png' },
@@ -12,16 +16,16 @@ const SideBar = () => {
         { label: '프로필', icon: '/img/pochaco.jpg' },
       ];
 
-      
+
     return (
-        <nav className="fixed top-0 left-0 h-screen 
+        <nav className="fixed top-0 left-0 h-screen
         w-18 hidden md:flex sidebarpoint:w-[16rem]
-         bg-white border-r border-gray-300 
+         bg-white border-r border-gray-300
          flex-col py-4">
         <div className="mb-6.5 px-7 py-7">
-          <img src="/img/icons/instagram_line_icon.png" alt="Instagram" 
+          <img src="/img/icons/instagram_line_icon.png" alt="Instagram"
           className="h-6 sidebarpoint:hidden" />
-          <img src="/img/icons/instagram_text_logo.png" alt="Instagram" 
+          <img src="/img/icons/instagram_text_logo.png" alt="Instagram"
           className="hidden sidebarpoint:block h-7" />
         </div>
         <div className="flex px-3 flex-col space-y-4 sidebarpoint:pl-4">
@@ -29,8 +33,9 @@ const SideBar = () => {
             <a
               key={idx}
               href="#"
-              className="flex items-center space-x-4 
+              className="flex items-center space-x-4
               hover:bg-gray-100 px-4 py-2 rounded"
+              onClick={item.onClick}
             >
               <img src={item.icon} alt={item.label} className="h-6" />
               <span className="hidden sidebarpoint:inline text-sm">
@@ -42,6 +47,5 @@ const SideBar = () => {
       </nav>
     );
   };
-  
+
   export default SideBar;
-  
