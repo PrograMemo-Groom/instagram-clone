74868aed84969312176bed97d759125f4bd02295
newfly101-local <newfly101@naver.com>
2024-12-27 17:26:04 +0900

    ♻️ feat: component로 분리 작업 진행 (input box) - valid에 따라 빨간색 테두리로 변경 refs: #39
    Signed-off-by: newfly101-local <newfly101@naver.com>

--- src/components/login/InputBox.jsx
+++ src/components/login/InputBox.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const InputBox = ({inputValue = "", setInputValue, id = "", type, label, children}) => {
+const InputBox = ({
+                      inputValue = "",
+                      setInputValue,
+                      id = "",
+                      isValid = "true",
+                      errorMessage = "",
+                      type,
+                      label,
+                      children
+                  }) => {
     return (
-        <label className="flex flex-col relative w-[250px] border border-gray-500 mb-2" htmlFor={id}>
+        <label
+            className={`flex flex-col relative w-[250px] border border-gray-500 mb-2 ${isValid ? "" : "border-red-500"}`}
+            htmlFor={id}>
             <span
                 className={`absolute text-gray-500 font-thin text-[8px] ml-1 mt-1 ${inputValue.trim().length === 0 ? "hidden" : ""} select-none`}>{label}</span>
             <input
@@ -11,6 +22,9 @@ const InputBox = ({inputValue = "", setInputValue, id = "", type, label, childre
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
             />
+            {!isValid && errorMessage && (
+                <p className="text-red-500 text-[10px] mt-1 pl-1">{errorMessage}</p>
+            )}
             {children && children}
         </label>
     );
