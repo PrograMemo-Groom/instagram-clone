e27c58b4f85bdcf69a1547dfb9850144b6cb0668
S2ej1n <ks320205@naver.com>
2024-12-27 18:26:46 +0900

    💄 메인 레이아웃 작업 (시맨틱태그)

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,10 +2,16 @@
 
 const MainPage = () => {
     return (
-
-        <div style={{fontSize: "50px"}}>
-            MainPage
-        </div>
+        // 메인 - (스토리, 포스트) / 오른쪽 사이드바) 
+        <main>
+            <section style={{backgroundColor:"lightblue"}}> 왼쪽 섹션 </section>
+            <aside style={{backgroundColor:"pink"}}>
+                오른쪽 사이드바
+                <footer>
+                    푸터
+                </footer>
+            </aside>
+        </main>
     );
 };
 
