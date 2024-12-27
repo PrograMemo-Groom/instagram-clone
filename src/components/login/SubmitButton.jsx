e9bb3c5c61116394514cb89d4e3e70f733b72cce
newfly101-local <newfly101@naver.com>
2024-12-28 00:21:47 +0900

    ♻️ feat: component customise 추가 refs: #39
    Signed-off-by: newfly101-local <newfly101@naver.com>

--- src/components/login/SubmitButton.jsx
+++ src/components/login/SubmitButton.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import {validateFields} from "@/components/login/validateFields.js";
 
-const SubmitButton = ({userId = "", userPw = "", userName = "", userRealName = "", text = ""}) => {
+const SubmitButton = ({
+                          userId = "",
+                          userPw = "",
+                          userName = "",
+                          userRealName = "",
+                          text = "",
+                          addCss = "",
+                          ...props
+                      }) => {
+    const type = text === "가입" || text === "로그인" ? "submit" : "text";
     const isDisabledForLogin = validateFields({
         userId: userId,
         userPw: userPw,
@@ -13,9 +22,10 @@ const SubmitButton = ({userId = "", userPw = "", userName = "", userRealName = "
         userRealName: userRealName
     });
     return (
-        <button type="submit"
-                className=" w-[250px] h-[30px] rounded-[5px] border bg-sky-500 hover:bg-blue-500 disabled:bg-sky-400 border-gray-200 disabled:border-gray-200 text-white text-[14px] font-semibold"
-                disabled={text === "로그인" ? isDisabledForLogin : isDisabledForJoin}
+        <button type={type}
+                {...props}
+                className={`${addCss} w-[250px] h-[30px] rounded-[5px] border bg-sky-500 hover:bg-blue-500 disabled:bg-sky-400 border-gray-200 disabled:border-gray-200 text-white text-[14px] font-semibold`}
+                disabled={text === "로그인" ? isDisabledForLogin : text === "가입" ? isDisabledForJoin : ""}
         >{text}
         </button>
     );
