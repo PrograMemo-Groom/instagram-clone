5b60f28ce12a300dbdc7d2fffdbcca592611dd77
newfly101-local <newfly101@naver.com>
2024-12-28 00:22:13 +0900

    💄 feat: input bg 속성 값 추가
    Signed-off-by: newfly101-local <newfly101@naver.com>

--- src/components/login/InputBox.jsx
+++ src/components/login/InputBox.jsx
@@ -17,7 +17,7 @@ const InputBox = ({
             <span
                 className={`absolute text-gray-500 font-thin text-[8px] ml-1 mt-1 ${inputValue.trim().length === 0 ? "hidden" : ""} select-none`}>{label}</span>
             <input
-                className={`outline-0 w-full h-[30px] pl-1 text-[10px] ${inputValue.trim().length === 0 ? "" : "pt-3"}`}
+                className={`outline-0 w-full h-[30px] pl-1 text-[10px] bg-gray-50 ${inputValue.trim().length === 0 ? "" : "pt-3"}`}
                 type={type} id={id} placeholder={label}
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
