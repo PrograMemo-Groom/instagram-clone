b7d367c055165170986d48f7dfa101cc6e6b93cf
newfly101-local <newfly101@naver.com>
2024-12-28 11:08:33 +0900

    💄 feat: more box 옵션값 복구
    Signed-off-by: newfly101-local <newfly101@naver.com>

--- src/components/login/MoreBox.jsx
+++ src/components/login/MoreBox.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const MoreBox = ({margin, width, contentWidth}) => {
+const MoreBox = () => {
     return (
-        <div className={`flex flex-row w-[${contentWidth}] mt-4 mb-4`}>
-            <div className={`flex ml-[${margin}] mr-[${margin}] w-[${width}] h-[12px] border-b border-b-gray-500`}/>
-            <div className={`flex w-[${contentWidth - width * 2}] text-[13px] justify-center`}>또는</div>
-            <div className={`flex ml-[${margin}] mr-[${margin}] w-[${width}] h-[12px] border-b border-b-gray-500`}/>
+        <div className={`flex flex-row w-[250px] mt-4 mb-4`}>
+            <div className={`flex ml-[5px] mr-[5px] w-[100px] h-[12px] border-b border-b-gray-500`}/>
+            <div className={`flex w-[30px] text-[13px] justify-center`}>또는</div>
+            <div className={`flex ml-[5px] mr-[5px] w-[100px] h-[12px] border-b border-b-gray-500`}/>
         </div>
     );
 };
