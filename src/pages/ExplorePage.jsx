3143fff6483e867c05f80b3e32b4e954b23d785a
yoorym-kim <twilight99me@gmail.com>
2024-12-28 19:43:59 +0900

    ➕더미데이터 추가

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -3,10 +3,115 @@
 const ExplorePage = () => {
 
     return (
-        <div>
-            ExplorePage
+        <div className={"explore-container"}>
+            <header className={"header"}>
+
+            </header>
+            <main className={"main"}>
+                <div className={"main-search"}>
+
+                </div>
+                <main className={"main-container"}>
+                    <div>
+
+                    </div>
+                </main>
+            </main>
         </div>
     );
 };
 
 export default ExplorePage;
+
+const data = [
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "VIDEO",
+        //"media_url": "https://example.com/video.mp4",
+        "thumbnail_url": "/assets/images/reelsImg.jpg",
+        "comments_count": 5
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "VIDEO",
+        //"media_url": "https://example.com/video.mp4",
+        "thumbnail_url": "/assets/images/reelsImg.jpg",
+        "comments_count": 5
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/videoPostImg.jpg",
+        "thumbnail_url": null,
+        "comments_count": 32
+    },
+    {
+        "media_type": "IMAGE",
+        "media_url": "/assets/images/postImg2.jpg",
+        "thumbnail_url": null,
+        "comments_count": 108
+    },
+    {
+        "media_type": "VIDEO",
+        //"media_url": "https://example.com/video.mp4",
+        "thumbnail_url": "/assets/images/reelsImg.jpg",
+        "comments_count": 5
+    },
+]
