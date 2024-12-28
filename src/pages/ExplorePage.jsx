6fa591223907191aef3a2eeb919ab5b6e83868ad
yoorym-kim <twilight99me@gmail.com>
2024-12-28 21:04:57 +0900

    💄탐색탭 기본구조 생성 + 릴스위치가 데이터 세트마다 번갈아가며 나오도록 설정

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -5,17 +5,54 @@ const ExplorePage = () => {
     return (
         <div className={"explore-container"}>
             <header className={"header"}>
-
+                헤더 검색창
             </header>
             <main className={"main"}>
                 <div className={"main-search"}>
-
+                    검색창
                 </div>
-                <main className={"main-container"}>
-                    <div>
+                <div className={"main-container"}>
+                    <div className="grid grid-cols-3 gap-1">
+                        {data.map((item, index) => {
+                            const setIndex = Math.floor(index/5 + 1); // 세트 번호 (5개의 아이템마다 1세트)
+                            const isReelsRight = setIndex % 2 === 1; // 홀수번째 세트: 릴스가 오른쪽, 짝수번째 세트 : 릴스가 왼쪽
 
+                            if (item.media_type === "VIDEO") {
+                                // 릴스 렌더링
+                                return (
+                                    <div
+                                        key={index}
+                                        className={`col-span-1 row-span-2 ${
+                                            isReelsRight ? "col-start-3" : "col-start-1"
+                                        } w-80 h-160 bg-gray-300`}
+                                    >
+                                        <img
+                                            src={item.thumbnail_url}
+                                            alt={`릴스 : ${index+1}`}
+                                            className="w-full h-full object-cover"
+                                        />
+                                    </div>
+                                );
+                            }
+
+                            else {
+                                // 게시물 렌더링
+                                return (
+                                    <div
+                                        key={index}
+                                        className="bg-gray-300 h-80 w-80"
+                                    >
+                                        <img
+                                            src={item.media_url}
+                                            alt={`게시물 : ${index + 1}`}
+                                            className="w-full h-full object-cover"
+                                        />
+                                    </div>
+                                );
+                            }
+                        })}
                     </div>
-                </main>
+                </div>
             </main>
         </div>
     );
@@ -37,6 +74,12 @@ const data = [
         "comments_count": 32
     },
     {
+        "media_type": "VIDEO",
+        //"media_url": "https://example.com/video.mp4",
+        "thumbnail_url": "/assets/images/reelsImg.jpg",
+        "comments_count": 5
+    },
+    {
         "media_type": "IMAGE",
         "media_url": "/assets/images/videoPostImg.jpg",
         "thumbnail_url": null,
@@ -79,12 +122,6 @@ const data = [
         "comments_count": 108
     },
     {
-        "media_type": "VIDEO",
-        //"media_url": "https://example.com/video.mp4",
-        "thumbnail_url": "/assets/images/reelsImg.jpg",
-        "comments_count": 5
-    },
-    {
         "media_type": "IMAGE",
         "media_url": "/assets/images/postImg2.jpg",
         "thumbnail_url": null,
@@ -97,6 +134,12 @@ const data = [
         "comments_count": 32
     },
     {
+        "media_type": "VIDEO",
+        //"media_url": "https://example.com/video.mp4",
+        "thumbnail_url": "/assets/images/reelsImg.jpg",
+        "comments_count": 5
+    },
+    {
         "media_type": "IMAGE",
         "media_url": "/assets/images/videoPostImg.jpg",
         "thumbnail_url": null,
@@ -107,11 +150,5 @@ const data = [
         "media_url": "/assets/images/postImg2.jpg",
         "thumbnail_url": null,
         "comments_count": 108
-    },
-    {
-        "media_type": "VIDEO",
-        //"media_url": "https://example.com/video.mp4",
-        "thumbnail_url": "/assets/images/reelsImg.jpg",
-        "comments_count": 5
     },
 ]
