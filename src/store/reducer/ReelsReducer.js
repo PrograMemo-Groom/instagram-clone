d40233410fa6ede3445ff8d9babdf3de97f89929
happygirlll <lbm000314@gmail.com>
2024-12-31 01:17:44 +0900

    🎨리덕스 리듀서에 댓글 설정 및 연결

--- src/store/reducer/ReelsReducer.js
+++ src/store/reducer/ReelsReducer.js
@@ -1,4 +1,5 @@
 import { SET_ACTIVE_MODAL, TOGGLE_LIKE } from "../type/ReelsActionType";
+import {SET_COMMENTS} from "@/store/action/ReelsAction.js";
 
 export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
 export const SET_REELS_DATA = "SET_REELS_DATA";
@@ -25,6 +26,7 @@ const initialState = {
     accessToken: "IGAAM2S8dbYxRBZAE1WanZAlN2tfYk9icVJ3V1BrekpJZAm5sdVBYOWEzaTYxajZADcGxialNjN1FhY3ZAqellwN215dXdUcm5TRlBnUmh1VHIyR1AxbjVPM2pzallHTm9JeEdKYTFYUnBFRUhZASjRTQlJTVEtWYTNlT3ctbWRWVm8wTQZDZD", // Access Token 추가
     reelsData: [], // Reels 데이터를 저장할 상태 추가
     userProfile: null, // 사용자 프로필 상태 추가
+    comments: {}, //댓글
 };
 
 const reelsReducer = (state = initialState, action) => {
@@ -53,6 +55,14 @@ const reelsReducer = (state = initialState, action) => {
             return {
                 ...state,
                 userProfile: action.payload,
+            };
+        case SET_COMMENTS:
+            return {
+                ...state,
+                comments: {
+                    ...state.comments,
+                    [action.payload.mediaId]: action.payload.comments,
+                },
             };
         default:
             return state;
