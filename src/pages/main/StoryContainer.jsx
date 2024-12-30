63449d18d89afc1487cba36b5cc9a5fbd00c8e08
S2ej1n <ks320205@naver.com>
2024-12-30 23:39:42 +0900

    💄 feat:메인  - 스토리 컨테이너 justify-between

--- src/pages/main/StoryContainer.jsx
+++ src/pages/main/StoryContainer.jsx
@@ -1,13 +1,91 @@
 import ProfileBox from "./ProfileBox";
 
 const StoryContainer = () => {
-    return (
-       <div className="flex justify-between h-[84px] 
-       mt-[8px] mb-[24px] py-[8px] items-center"
-       style={{backgroundColor:"lightcoral"}}>
-         <ProfileBox />
-       </div>
-    );
+  return (
+    <div className="flex flex-row justify-between items-center h-[84px] mt-[8px] mb-[24px] py-[8px]">
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">내 스토리</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+      
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-[64px] h-[84px] px-[6px]">
+        {/* 프로필 배경 */}
+        <div className="h-[62px] w-[62px] bg-[#D301C5;] rounded-full  border border-[#E5E5E5]
+                flex justify-center items-center box-border mb-[2px]">
+          <img className="h-[56px] w-[56px] rounded-full border border-[#E5E5E5 object-cover"
+            src="/img/pochaco.jpg" />
+        </div>
+        <p className="text-main-story-size px-[2px]">ham_ster_</p>
+      </div>
+
+      {/* <ProfileBox /> */}
+    </div>
+  );
 };
 
 export default StoryContainer;
