fb1bbc4ae6d2a2c2b1175a5b0abd8ad6047c3ae5
S2ej1n <ks320205@naver.com>
2024-12-31 01:02:51 +0900

    💄 feat:메인- 좋아요 및 포스트, 댓글섹션

--- src/pages/main/PostAction.jsx
+++ src/pages/main/PostAction.jsx
@@ -3,7 +3,7 @@ const PostAction = () => {
        <div className="flex flex-col">
         <section className="flex my-[4px] items-center justify-between">
             <div>
-                <button className="h-[40px] p-[8px]">
+                <button className="h-[40px] py-[8px] pr-[8px]">
                     <img className="h-[24px]" src="/img/icons/free-icon-heart-1077035.png"/>
                 </button>
                 <button className="h-[40px] p-[8px]">
@@ -19,9 +19,15 @@ const PostAction = () => {
                 </button>
             </div>
         </section>
-        <div>
-            df
+        <div className="text-[14px] font-bold">
+            <span>좋아요 <span>137.9만</span>개</span>
         </div>
+        <div className="text-[14px] my-[4px]">
+            <span className="mr-[6px] font-bold">xfhif_shd_djj</span><span>우아아아</span>
+        </div>
+
+        {/* 댓글달면 게시 버튼 뜨게 수정해야함 */}
+        <textarea className="text-[14px] h-[18px] focus:outline-none resize-none" placeholder="댓글 달기..."></textarea>
        </div>
     );
 };
