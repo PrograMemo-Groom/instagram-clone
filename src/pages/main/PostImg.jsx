97e553be17cbbc94349f9c581bc83abd3e409445
S2ej1n <ks320205@naver.com>
2024-12-31 00:36:59 +0900

    💄 feat:메인- 포스트 컨테이너 이미지 시멘틱태그 수정

--- src/pages/main/PostImg.jsx
+++ src/pages/main/PostImg.jsx
@@ -1,9 +1,9 @@
 // 슬라이드 기능 구현하기
 const PostImg = () => {
     return (
-       <div className="flex rounded-sm w-[468px] min-h-[280px] max-h-[585px] overflow-y-hidden"
+       <section className="flex rounded-sm w-[468px] min-h-[280px] max-h-[585px] overflow-y-hidden"
         style={{backgroundColor:"gray"}}>
-       </div>
+       </section>
     );
 };
 
