ba9e84306ccb3b3b2a3f66002ed43e4c1303e21c
S2ej1n <ks320205@naver.com>
2024-12-30 23:50:49 +0900

    💄 feat:메인- 포스트 컨테이너 구성나누기

--- src/pages/main/PostContainer.jsx
+++ src/pages/main/PostContainer.jsx
@@ -3,8 +3,9 @@ const PostContainer = () => {
        <div className="flex justify-center">
          <div className="w-[470px] pb-[16px] mb-[20px] border-b-2 border-[#DBDBDB]"
           style={{backgroundColor:"white"}}>
-          PostContainer
-          <p>dd</p>
+          <p>프로필 정보 박스</p>
+          <p>포스트 박스</p>
+          <p>좋아요랑 댓글박스</p>
          </div>
        </div>
     );
