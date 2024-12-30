a3b360f638838bafefe58b2512b7e0399585ab40
happygirlll <lbm000314@gmail.com>
2024-12-31 00:07:41 +0900

    🎨redux에 사용자 프로필 이미지 데이터 추가

--- src/store/reducer/ReelsReducer.js
+++ src/store/reducer/ReelsReducer.js
@@ -2,6 +2,7 @@ import { SET_ACTIVE_MODAL, TOGGLE_LIKE } from "../type/ReelsActionType";
 
 export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
 export const SET_REELS_DATA = "SET_REELS_DATA";
+export const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 export const setAccessToken = (token) => ({
     type: SET_ACCESS_TOKEN,
@@ -13,11 +14,17 @@ export const setReelsData = (data) => ({
     payload: data,
 });
 
+export const setUserProfile = (profile) => ({
+    type: SET_USER_PROFILE,
+    payload: profile,
+});
+
 const initialState = {
     isLiked: false,
     activeModal: null, // 현재 활성화된 모달
     accessToken: "IGAAM2S8dbYxRBZAE1WanZAlN2tfYk9icVJ3V1BrekpJZAm5sdVBYOWEzaTYxajZADcGxialNjN1FhY3ZAqellwN215dXdUcm5TRlBnUmh1VHIyR1AxbjVPM2pzallHTm9JeEdKYTFYUnBFRUhZASjRTQlJTVEtWYTNlT3ctbWRWVm8wTQZDZD", // Access Token 추가
     reelsData: [], // Reels 데이터를 저장할 상태 추가
+    userProfile: null, // 사용자 프로필 상태 추가
 };
 
 const reelsReducer = (state = initialState, action) => {
@@ -41,6 +48,11 @@ const reelsReducer = (state = initialState, action) => {
             return {
                 ...state,
                 reelsData: action.payload,
+            };
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                userProfile: action.payload,
             };
         default:
             return state;
