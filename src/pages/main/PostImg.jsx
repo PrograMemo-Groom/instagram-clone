bd1d898be062dc9c507b0f2e155dc78da4d92466
S2ej1n <ks320205@naver.com>
2024-12-31 00:41:06 +0900

    💄 feat:메인- 포스트 컨테이너 이미지 테스트 완료

--- src/pages/main/PostImg.jsx
+++ src/pages/main/PostImg.jsx
@@ -3,6 +3,7 @@ const PostImg = () => {
     return (
        <section className="flex rounded-sm w-[468px] min-h-[280px] max-h-[585px] overflow-y-hidden"
         style={{backgroundColor:"gray"}}>
+        <img src="https://www.harpersbazaar.co.kr/resources/online/online_image/2024/11/12/11741d75-4d22-4af1-846c-3341929eb03b.jpg"/>
        </section>
     );
 };
