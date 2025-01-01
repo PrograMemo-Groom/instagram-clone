87cbcdf504466ce91e2ccd05f47a9d2e60275710
happygirlll <lbm000314@gmail.com>
2025-01-01 23:45:53 +0900

    💄릴스 댓글 좋아요 추가

--- src/pages/Reels/ReelsComment.jsx
+++ src/pages/Reels/ReelsComment.jsx
@@ -69,6 +69,13 @@ const ReelsComment = ({ mediaId, onClose }) => {
                                 </div>
                                 <p className="text-sm">{comment.text}</p>
                             </div>
+                            <div className="flex items-center justify-center">
+                                <img
+                                    src="/assets/reels/like_btn2.svg"
+                                    alt="좋아요"
+                                    className="w-5 h-5 mr-3 cursor-pointer"
+                                />
+                            </div>
                         </div>
                     ))
                 ) : (
