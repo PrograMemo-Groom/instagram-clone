09a0370cc7077966e4a7359abb8fd05c1032db06
saaad99 <ehdgnstla@naver.com>
2025-01-04 01:05:06 +0900

    :lipstick: 새 게시물 만들기 UI 대충 완료

--- src/pages/PostModal.jsx
+++ src/pages/PostModal.jsx
@@ -4,6 +4,15 @@ const PostModal = () => {
     const [pageIndex, setPageIndex] = useState(0); // 현재 페이지 인덱스
     const fileInputRef = useRef(null);
     const [content, setContent] = useState("");
+    const [text, setText] = useState(""); // 입력된 텍스트 상태
+
+    const handleTextChange = (e) => {
+        const inputText = e.target.value;
+        // 입력된 텍스트가 최대 문자 수를 초과하지 않도록 제한
+        if (inputText.length <= 2200) {
+            setText(inputText);
+        }
+    };
 
     const handleButtonClick = () => {
         fileInputRef.current.click(); // 버튼 클릭 시 input을 실행
@@ -35,107 +44,149 @@ const PostModal = () => {
         e.preventDefault(); // 기본 드래그 오버 동작 방지
     };
 
-    const filterData = [
-        {id: 1, image: "/img/pochaco.jpg", description: "필터 1"},
-        {id: 2, image: "/img/pochaco.jpg", description: "필터 2"},
-        {id: 3, image: "/img/pochaco.jpg", description: "필터 3"},
-        {id: 4, image: "/img/pochaco.jpg", description: "필터 4"},
-        {id: 5, image: "/img/pochaco.jpg", description: "필터 5"},
-        {id: 6, image: "/img/pochaco.jpg", description: "필터 6"},
-    ];
+    const filterData = [{id: 1, image: "/img/pochaco.jpg", description: "필터 1"}, {
+        id: 2, image: "/img/pochaco.jpg", description: "필터 2"
+    }, {id: 3, image: "/img/pochaco.jpg", description: "필터 3"}, {
+        id: 4, image: "/img/pochaco.jpg", description: "필터 4"
+    }, {id: 5, image: "/img/pochaco.jpg", description: "필터 5"}, {
+        id: 6, image: "/img/pochaco.jpg", description: "필터 6"
+    },];
 
-    const pages = [
-        {
-            title: "새 게시물 만들기",
-            content: (
-                <div
-                    className="flex-grow flex flex-col items-center justify-center space-y-3 border-1 "
-                    onDrop={handleDrop}
-                    onDragOver={handleDragOver}
-                >
-                    <img
-                        className="h-40 w-40 object-contain"
-                        src="assets/icon/selectImg.svg"
-                        alt="이미지 선택"
-                    />
-                    <p className="text-black">사진과 동영상을 여기다 끌어다 놓으세요</p>
-                    <button
-                        onClick={() => handleButtonClick()}
-                        className="px-4 py-1 bg-blue-400 text-white rounded hover:bg-blue-500"
-                    >
-                        컴퓨터에서 선택
+    const pages = [{
+        title: "새 게시물 만들기", content: (<div
+            className="flex-grow flex flex-col items-center justify-center space-y-3 border-1 "
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+        >
+            <img
+                className="h-40 w-40 object-contain"
+                src="assets/icon/selectImg.svg"
+                alt="이미지 선택"
+            />
+            <p className="text-black">사진과 동영상을 여기다 끌어다 놓으세요</p>
+            <button
+                onClick={() => handleButtonClick()}
+                className="px-4 py-1 bg-blue-400 text-white rounded hover:bg-blue-500"
+            >
+                컴퓨터에서 선택
+            </button>
+            <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*, video/*"
+                multiple
+                className="hidden"
+                onChange={(e) => handleFileChange(e)}
+            />
+        </div>),
+    }, {
+        title: "자르기", content: (<div className="flex items-center justify-center">
+            <img src={content}
+                 className="w-full h-full max-w-[855px] max-h-[855px]  object-cover md:aspect-[1/1] xl:aspect-[1/1]"/>
+        </div>),
+    }, {
+        title: "편집", content: (<div className="flex flex-wrap">
+            {/* 좌측 이미지 영역 */}
+            <div className="w-full max-w-[855px] h-full flex-shrink-0">
+                <img
+                    src={content}
+                    alt="이미지"
+                    className="w-full h-full object-cover md:aspect-[1/1] xl:aspect-[1/1]"
+                />
+            </div>
+
+            {/* 우측 편집 영역 */}
+            <div className="w-full md:w-full lg:w-[340px] h-full border-l-2 flex-shrink-0">
+                {/* 버튼 */}
+                <div className="w-full flex mb-4 border-b-2">
+                    <button className="py-1 text-black border-r-2 w-1/2">
+                        필터
                     </button>
-                    <input
-                        ref={fileInputRef}
-                        type="file"
-                        accept="image/*, video/*"
-                        multiple
-                        className="hidden"
-                        onChange={(e) => handleFileChange(e)}
+                    <button className="py-1 text-black w-1/2">
+                        조정
+                    </button>
+                </div>
+
+                <div className="w-full grid grid-cols-3 gap-4 p-2">
+                    {filterData.map(item => (<div key={item.id} className="flex flex-col items-center">
+                        <img
+                            src={item.image}
+                            alt={item.description}
+                            className="w-24 h-24 object-contain mb-2 border-2"
+                        />
+                        <p className="text-black">{item.description}</p>
+                    </div>))}
+                </div>
+            </div>
+        </div>),
+    }, {
+        title: "새 게시물 만들기",
+        content: (
+            <div className="flex">
+                {/* 좌측 이미지 영역 */}
+                <div className="w-full max-w-[855px] h-full flex-shrink-0">
+                    <img
+                        src={content}
+                        alt="이미지"
+                        className="w-full h-full object-cover md:aspect-[1/1] xl:aspect-[1/1]"
                     />
                 </div>
-            ),
-        },
-        {
-            title: "자르기",
-            content: (
-                <div className="flex items-center justify-center">
-                    <img src={content}/>
-                </div>
-            ),
-        },
-        {
-            title: "편집",
-            content: (
-                <div className="flex flex-wrap">
-                    {/* 좌측 이미지 영역 */}
-                    <div className="w-full max-w-[855px] h-full flex-shrink-0">
+
+                {/* 우측 편집 영역 */}
+                <div className="flex flex-col flex-grow w-full h-full border">
+                    {/* 유저 네임 영역 */}
+                    <div className="w-full gap-4 flex justify-start p-2">
+            <span className="rounded-full border border-gray-400">
+                <img src="/img/pochaco.jpg" className="w-8 h-8 rounded-full"/>
+            </span>
+                        <span className="py-1 text-black">ddonghun__</span>
+                    </div>
+
+                    {/* 텍스트 입력 영역 */}
+                    <textarea
+                        type="text"
+                        placeholder="내용을 입력하세요"
+                        onChange={handleTextChange}
+                        className="w-full h-[339px] p-2"
+                    ></textarea>
+
+                    {/* 텍스트 상태 및 아이콘 */}
+                    <div className="flex justify-between p-2 text-gray-700 border border-t-gray-200">
                         <img
-                            src={content}
-                            alt="이미지"
-                            className="w-full h-full object-cover md:aspect-[1/1] xl:aspect-[1/1]"
+                            src="/assets/icon/smile.svg"
+                            className="w-6 h-6 cursor-pointer"
                         />
+                        <span>{text.length} / 2200</span>
                     </div>
 
-                    {/* 우측 편집 영역 */}
-                    <div className="w-full md:w-full lg:w-[340px] h-full border-l-2 flex-shrink-0">
-                        {/* 버튼 */}
-                        <div className="w-full flex mb-4 border-b-2">
-                            <button className="py-1 text-black border-r-2 w-1/2">
-                                필터
-                            </button>
-                            <button className="py-1 text-black w-1/2">
-                                조정
-                            </button>
-                        </div>
-
-                        <div className="w-full grid grid-cols-3 gap-4 p-2">
-                            {filterData.map(item => (
-                                <div key={item.id} className="flex flex-col items-center">
-                                    <img
-                                        src={item.image}
-                                        alt={item.description}
-                                        className="w-24 h-24 object-contain mb-2 border-2"
-                                    />
-                                    <p className="text-black">{item.description}</p>
-                                </div>
-                            ))}
-                        </div>
+                    {/* 옵션 영역 */}
+                    <div className="flex flex-col gap-4 pt-2 ">
+                        <button
+                            className="flex items-center justify-between w-full px-4 py-2 text-black">
+                            위치 추가
+                            <img src="/assets/icon/locationPin.svg" alt="위치 아이콘" className="w-5 h-5"/>
+                        </button>
+                        <button
+                            className="flex items-center justify-between w-full px-4 py-2 text-black">
+                            공동 작업자 추가
+                            <img src="/assets/icon/tagUser.svg" alt="공동 작업자 아이콘" className="w-5 h-5"/>
+                        </button>
+                        <button
+                            className="flex items-center justify-between w-full px-4 py-2 text-black">
+                            접근성
+                            <img src="/assets/icon/dropdown.svg" alt="접근성 아이콘" className="w-5 h-5"/>
+                        </button>
+                        <button
+                            className="flex items-center justify-between w-full px-4 py-2 text-black">
+                            고급 설정
+                            <img src="/assets/icon/dropdown.svg" alt="고급 설정 아이콘" className="w-5 h-5"/>
+                        </button>
                     </div>
                 </div>
+            </div>
 
-
-            ),
-        },
-        {
-            title: "새 게시물 만들기",
-            content: (
-                <div className="flex-grow flex items-center justify-center">
-                    <p className="text-black">최종 게시물 작성 페이지입니다.</p>
-                </div>
-            ),
-        },
-    ];
+        ),
+    },];
 
     const handleNext = () => {
         if (pageIndex < pages.length - 1) {
@@ -149,46 +200,38 @@ const PostModal = () => {
         }
     };
 
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div
-                className="flex flex-col bg-white rounded-lg shadow-lg min-h-[60%] h-fit  min-w-[60%] w-[1200px]">
+    return (<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-200">
+        <div
+            className="flex flex-col bg-white rounded-lg shadow-lg min-h-[60%] h-fit  min-w-[60%] w-[1200px]">
 
-                {/* 상단 */}
-                <h2 className="flex justify-between items-center border-b-2 p-2 font-bold">
-                    {pageIndex > 0 && (
-                        <button
-                            onClick={handlePrev}
-                            className="px-2 py-1 rounded bg-blue-400 text-white hover:bg-blue-500"
-                        >
-                            이전
-                        </button>
-                    )}
-                    {/* 모달 제목 중앙에 위치 */}
-                    <span className="flex-grow text-center">{pages[pageIndex].title}</span>
-                    {pageIndex > 0 && pageIndex < pages.length - 1 && (
-                        <button
-                            onClick={handleNext}
-                            className="px-2 py-1 rounded bg-blue-400 text-white hover:bg-blue-500"
-                        >
-                            다음
-                        </button>
-                    )}
-                    {pageIndex === pages.length - 1 && (
-                        <button
-                            onClick={() => console.log("게시물 공유")}
-                            className="px-4 py-1 rounded bg-green-500 text-white hover:bg-green-600"
-                        >
-                            공유하기
-                        </button>
-                    )}
-                </h2>
+            {/* 상단 */}
+            <h2 className="flex justify-between items-center border-b-2 p-2 font-bold">
+                {pageIndex > 0 && (<button
+                    onClick={handlePrev}
+                    className="px-2 py-1 rounded bg-blue-400 text-white hover:bg-blue-500"
+                >
+                    이전
+                </button>)}
+                {/* 모달 제목 중앙에 위치 */}
+                <span className="flex-grow text-center">{pages[pageIndex].title}</span>
+                {pageIndex > 0 && pageIndex < pages.length - 1 && (<button
+                    onClick={handleNext}
+                    className="px-2 py-1 rounded bg-blue-400 text-white hover:bg-blue-500"
+                >
+                    다음
+                </button>)}
+                {pageIndex === pages.length - 1 && (<button
+                    onClick={() => console.log("게시물 공유")}
+                    className="px-4 py-1 rounded bg-green-500 text-white hover:bg-green-600"
+                >
+                    공유하기
+                </button>)}
+            </h2>
 
-                {/* 현재 페이지 콘텐츠 */}
-                {pages[pageIndex].content}
-            </div>
+            {/* 현재 페이지 콘텐츠 */}
+            {pages[pageIndex].content}
         </div>
-    );
+    </div>);
 };
 
 export default PostModal;
